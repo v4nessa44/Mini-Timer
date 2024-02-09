f5926ba0d8f486773f862f77150828e4cca8490a
v4nessa04 <v4nessa04@gmail.com>
2024-02-09 18:58:59 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let input = document.querySelector("input");
+let startBtn = document.querySelector("button");
+let stopwatch = document.querySelector(".number");
+
+
+let time = 0;
+let timeInterval;
+
+function showElementInUi(){
+    time--;
+    stopwatch.innerHTML = time;
+
+    if(time === 0){
+        clearInterval(timeInterval)
+    }
+}
+
+function startStopWatch(){
+    time = input.value;
+
+    
+
+    timeInterval = setInterval(showElementInUi, 1000);
+}
+
+
+
+
+startBtn.addEventListener("click", startStopWatch);
